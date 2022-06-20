34553e23af5d8fd5bbed1980a7838806a030b898
Jorge Mohedano <jorge.mohedano88@gmail.com>
2022-06-21 01:41:38 +0200

    Configuracion 2 monitores

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,12 +3,9 @@
 dex $HOME/.config/autostart/arcolinux-welcome-app.desktop &
 
 # Configuración monitores
-#xrandr --output eDP-1 --mode 1920x1080
-#xrandr --output HDMI-2 --mode 1920x1080 --above eDP-1 --primary
 xrandr --output eDP-1 --mode 1920x1080 --fb 1728x972 --panning 1728x972 --scale 0.9x0.9
-xrandr --output HDMI-2 --above eDP-1
-#randr --output HDMI-2 --primary
+xrandr --output HDMI-2 --mode 1920x1080 --rate 120 --above eDP-1
 
 # Arreglar tamaño cursor
-xrdb ~/.Xresources
-exec qtile
+#xrdb ~/.Xresources
+#exec qtile
